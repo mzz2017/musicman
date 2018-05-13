f5404bd3e812297509b78a6d61ef13f59f5a8e01
mzz2017 <m@mzz.pub>
2018-05-13 14:06:31 +0800

    fix(app.js): 修复一直status400bug

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ app.use('/api', apiRoutes)
 const PORT = 9000
 app.post('/api/search', jsonParser, (req, res) => {
   let body = req.body
-  if (body[0] === undefined) {
+  if (body.songName === undefined) {
     res.status(400).send('')
   }
+  if (!body.page)
+    body.page = 0
   // console.log(body)
   let url = `http://c.y.qq.com/soso/fcgi-bin/client_search_cp?ct=24&qqmusic_ver=1298&new_json=1&remoteplace=txt.yqq.center&t=0&aggr=1&cr=1&catZhida=1&lossless=0&flag_qc=0&p=${body.page}&n=30&w=${body.songName}&&jsonpCallback=searchCallbacksong2020&format=json&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0`
   axios({
