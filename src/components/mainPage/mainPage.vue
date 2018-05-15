b0b19ec0c9142a093d9253fbdc841549c008fb66
mzz2017 <m@mzz.pub>
2018-05-15 14:05:33 +0800

    feat: 增加下载框，下载文件自动更名

--- src/components/mainPage/mainPage.vue
+++ src/components/mainPage/mainPage.vue
@@ -34,7 +34,8 @@
                      :disabled="!val"
                      class="formatIcon"
                      :key="format.value"
-                     @click="handleOnClickDownload(format,scope.row.strMediaMid)">{{format}}
+                     @click="handleOnClickDownload(format,scope.row.strMediaMid,scope.row.name,scope.row.singer)">
+            {{format}}
           </el-button>
         </template>
       </el-table-column>
@@ -47,11 +48,27 @@
         :page-size=15>
       </el-pagination>
     </div>
+    <el-dialog :title="dialogDownloadTitle"
+               :visible.sync="dialogDownloadVisible"
+               width="30%"
+               :close-on-click-modal="false"
+               :close-on-press-escape="false"
+               :before-close="handleBeforeCloseDialogDownload">
+      <div id="download">
+        <el-progress :width="100"
+                     type="circle"
+                     :percentage="downloadProgress"
+                     :status="downloadStatus"
+        ></el-progress>
+        <div id="downloadSpeed">{{downloadSpeed}} kb/s</div>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
   import axios from 'axios'
+  import download from '../../common/js/download'
 
   export default {
     name: "mainPage",
@@ -78,14 +95,66 @@
           },
         },
         totalNum: 0,
-        searchContentKeeper: ''
+        searchContentKeeper: '',
+        dialogDownloadVisible: false,
+        dialogDownloadTitle: '成都-赵雷',
+        downloadProgress: 0,
+        downloadStatus: '',
+        downloadSpeed: 0,
+        downloadAbort: false
       }
     },
     methods: {
-      handleOnClickDownload(format, strMediaMid) {
+      handleOnClickDownload(format, strMediaMid, songName, singer) {
+        //获取vkey
         let vkey = window.localStorage.getItem('vkey')
+        //合成下载链接
         let url = `http://streamoc.music.tc.qq.com/${this.formatMap[format].prefix}00${strMediaMid}.${this.formatMap[format].suffix}?vkey=${vkey}&guid=1234567890&uin=1008611&fromtag=8`
-        window.open(url, '_blank')
+        //合成文件名
+        let savename = `${songName}-${singer}.${this.formatMap[format].suffix}`
+        //显示下载对话框
+        this.dialogDownloadVisible = true
+        this.dialogDownloadTitle = savename
+        //创建XMLHttpRequest对象
+        let req = new XMLHttpRequest();
+        //类型设为blob
+        req.responseType = "blob"
+        req.open("GET", url, true);
+        //初始化变量
+        let that = this
+        this.downloadAbort = false
+        this.downloadProgress = 0
+        this.downloadStatus = ''
+
+        let beginTime = Date.parse(new Date().toUTCString())
+        let lastCountTime = beginTime
+        //监听下载进度
+        req.addEventListener('progress', (event) => {
+          if (event.lengthComputable) {
+            if (that.downloadAbort) {
+              that.downloadAbort = false
+              req.abort()
+            }
+            that.downloadProgress = Math.floor(event.loaded / event.total * 100)
+            //取下载速度(B/ms, 即KB/s), 速度变动间隔为1s
+            let nowTime = Date.parse(new Date().toUTCString())
+            if (nowTime - lastCountTime > 500) {
+              lastCountTime = nowTime
+              that.downloadSpeed = Math.floor(event.loaded / (nowTime - beginTime))
+            }
+            console.log(event)
+          }
+        }, false)
+        //下载完毕事件
+        req.onreadystatechange = function () {
+          if (req.readyState === 4 && req.status === 200) {
+            let blob = new Blob([req.response], {type: 'application/force-download'})
+            download(blob, savename)
+            that.downloadStatus = 'success'
+          }
+        }
+        //敬请开始吧
+        req.send(null)
       },
       handleOnClickSearch(page) {
         //记录下这次搜索的内容，翻页的时候搜索不会因为实时更改的searchContent而改变搜索内容
@@ -129,13 +198,28 @@
               "strMediaMid": item.file.strMediaMid
             }
             array.push(object)
-            this.totalNum = response.data.data.song.totalnum
           }
+          this.totalNum = response.data.data.song.totalnum
           this.songList = array
           window.scrollTo(0, 0)
         }).catch((error) => {
           process.env.NODE_ENV === 'development' && console.log(error)
         })
+      },
+      handleBeforeCloseDialogDownload(done) {
+        //下载完毕则直接可以关闭
+        if (this.downloadStatus !== ''){
+          done()
+          return
+        }
+        this.$confirm('确认停止下载？')
+          .then(() => {
+            this.downloadAbort = true
+            done();
+          })
+          .catch((error) => {
+            process.env.NODE_ENV === 'development' && console.log(error)
+          });
       },
       getVkey() {
         axios({
@@ -156,11 +240,10 @@
         this.getVkey()
       }
       window.onkeyup = (e) => {
-        if (e === event && e.key==='Enter') {
+        if (e === event && e.key === 'Enter') {
           this.handleOnClickSearch()
         }
       }
-
     }
   }
 </script>
@@ -170,16 +253,22 @@
 </style>
 
 <style lang="scss">
+  //强行改写element-ui样式
   .songList {
     th {
-      text-align: center;
+      text-align: center !important;
     }
     .cell, .cell .formatIcon {
-      display: flex;
-      justify-content: center;
+      display: flex !important;
+      justify-content: center !important;
     }
     .invisible {
-      visibility: hidden;
+      visibility: hidden !important;
     }
+  }
+
+  .el-message-box {
+    vertical-align: top !important;
+    margin-top: 22vh !important;
   }
 </style>
