33cbe20081eda0c8f81ab6619c5cbb9eae6822a4
mzz2017 <m@mzz.pub>
2018-05-17 04:15:14 +0800

    chore: 取消了sourcemap, 给后端请求vkey的api加上代理以支持国外服务器

--- config/index.js
+++ config/index.js
@@ -20,7 +20,7 @@ module.exports = {
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-    
+
     /**
      * Source Maps
      */
@@ -49,7 +49,7 @@ module.exports = {
      * Source Maps
      */
 
-    productionSourceMap: true,
+    productionSourceMap: false,
     // https://webpack.js.org/configuration/devtool/#production
     devtool: '#source-map',
 
